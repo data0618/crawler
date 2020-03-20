a322c2ce4aeaef74b501dd0af6b82861adfc99b8
liujp <liujp@koal.com>
2020-03-20 14:40:04 +0800

    Initial commit

---
+++ src/main/java/com/ljp/crawler/CrawlerApplication.java
@@ -0,0 +1,14 @@
+package com.ljp.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class CrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerApplication.class, args);
+    }
+
+}
